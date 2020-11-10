388fb812121ea7235bec322888a6e0f507062b9b
Marco Gomes <antoniogomes2504@gmail.com>
2020-11-10 16:58:59 -0300

    correcão do botao adicionar

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ function Contador() {
   return (
     <div>
       <div>{contador}</div>
-      <button onClick={adicionarContador}> </button>Adicionar
+      <button onClick={adicionarContador}>Adicionar</button>
     </div>
   );
 }
